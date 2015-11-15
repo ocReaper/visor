207490feb76f7994949f0d8156e339b998ab2f67
ocReaper <ocreaper@gmail.com>
2015-11-15 16:46:30 +0100

    feat(visor.ui-router): stop module if $state is not available

--- release/visor.js
+++ release/visor.js
@@ -460,68 +460,70 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
         uiModuleExists = false;
       }
 
-      if (uiModuleExists) {
-        $injector.invoke(["$state", function ($state) {
-          //  we need to check parent states for permissions as well
-          visorPermissions.getPermissionsFromNext = function (next) {
-            var perms = []
-              , chain
-              , parent;
-
-            while (next) {
-              if (next.restrict) {
-                perms.unshift(next.restrict);
-              }
-
-              if (next.parent) {
-                next = $state.get(next.parent);
-              } else if (next.name.indexOf('.') > 0) {
-                chain = next.name.split('.');
-                // remove the leftmost
-                chain.pop();
-                parent = chain.join('.');
-                next = $state.get(parent);
-              } else {
-                next = null;
-              }
-            }
-            return perms;
-          };
-
-          toUrl = null;
-          bypass = false;
-
-          $rootScope.$on('$stateChangeStart', function (e, toState, toParams) {
-            var shouldContinue;
-
-            if (bypass) {
-              bypass = false;
-              return;
-            }
-            toUrl = $state.href(toState, toParams).replace(/^#/, '');
-            shouldContinue = visorPermissions.onRouteChange(toState, function delayChange(promise) {
-              promise.then(function () {
-                bypass = true;
-                $state.go(toState, toParams);
-              });
-            });
-            if (!shouldContinue || shouldContinue === 'delayed') {
-              e.preventDefault();
-            }
-          });
-          visorPermissions.invokeNotAllowed = function (notAllowed) {
-            // timeout is required because when using preventDefault on $stateChangeStart, the url is
-            // reverted to it's original location, and no change at this time will override this.
-            $timeout(function () {
-              $injector.invoke(notAllowed, null, {restrictedUrl: toUrl});
-            }, 0);
-          };
-
-          visorPermissions.getRoute = function (routeId) {
-            return $state.get(routeId);
-          };
-        }]);
+      if (!uiModuleExists) {
+        return angular.noop;
       }
+
+      $injector.invoke(["$state", function ($state) {
+        //  we need to check parent states for permissions as well
+        visorPermissions.getPermissionsFromNext = function (next) {
+          var perms = []
+            , chain
+            , parent;
+
+          while (next) {
+            if (next.restrict) {
+              perms.unshift(next.restrict);
+            }
+
+            if (next.parent) {
+              next = $state.get(next.parent);
+            } else if (next.name.indexOf('.') > 0) {
+              chain = next.name.split('.');
+              // remove the leftmost
+              chain.pop();
+              parent = chain.join('.');
+              next = $state.get(parent);
+            } else {
+              next = null;
+            }
+          }
+          return perms;
+        };
+
+        toUrl = null;
+        bypass = false;
+
+        $rootScope.$on('$stateChangeStart', function (e, toState, toParams) {
+          var shouldContinue;
+
+          if (bypass) {
+            bypass = false;
+            return;
+          }
+          toUrl = $state.href(toState, toParams).replace(/^#/, '');
+          shouldContinue = visorPermissions.onRouteChange(toState, function delayChange(promise) {
+            promise.then(function () {
+              bypass = true;
+              $state.go(toState, toParams);
+            });
+          });
+          if (!shouldContinue || shouldContinue === 'delayed') {
+            e.preventDefault();
+          }
+        });
+        visorPermissions.invokeNotAllowed = function (notAllowed) {
+          // timeout is required because when using preventDefault on $stateChangeStart, the url is
+          // reverted to it's original location, and no change at this time will override this.
+          $timeout(function () {
+            $injector.invoke(notAllowed, null, {restrictedUrl: toUrl});
+          }, 0);
+        };
+
+        visorPermissions.getRoute = function (routeId) {
+          return $state.get(routeId);
+        };
+      }]);
     }]);
 })();
 })(window, window.angular);
